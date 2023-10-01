f52665593040c093481b88ea7067c76f59e4a918
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-01 00:33:25 -0400

    ric.sh

--- ric.sh
+++ ric.sh
@@ -1,21 +1,30 @@
+echo -e "\e[1;34m"
+cat << "EOF"
+  ____                   _    _    _     _   
+ / ___|  ___   ___  _ __| |_ / |_ | |__ (_)_ 
+ \___ \ / _ \ / _ \| '__| __| | __|| '_ \| \ \
+  ___) |  __/|  __/| |  | |_| | |_ | | | | |\ \
+ |____/ \___| \___||_|   \__|_|\__||_| |_|_| \_\
+                                                
+EOF
+echo -e "\e[0m"
+echo -e "Welcome to \e[1;32mOAIC Setup\e[0m!"
+echo -e "Let's get started...\n"
 #!/bin/bash
 
 LOG_FILE="install.log"
 
-# Function to display a formatted message with a counter
 print_progress() {
   local message="$1"
   printf "\n\e[1;34m%s\e[0m\n" "${message}" | tee -a $LOG_FILE
 }
 
-# Function to log the output of a command
 log_command() {
   local cmd="$@"
   echo "$cmd" >> $LOG_FILE
   $cmd 2>&1 | tee -a $LOG_FILE
 }
 
-# Function to prompt user to continue or exit on error
 prompt_continue_or_exit() {
   echo -e "\n\nError occurred during installation." | tee -a $LOG_FILE
   read -p "Do you want to continue? (Y/N): " choice
@@ -33,43 +42,47 @@ update_packages() {
   print_progress "Package list updated successfully."
 }
 
-clone_and_setup_git() {
-  print_progress "Cloning the git repository..."
-  log_command git clone https://github.com/openaicellular/oaic.git
-  log_command cd oaic
-  log_command git submodule update --init --recursive --remote
-  log_command cd RIC-Deployment/tools/k8s/bin
-  log_command ./gen-cloud-init.sh
-  log_command sudo ./k8s-1node-cloud-init-k_1_16-h_2_17-d_cur.sh
-  log_command sudo kubectl get ns ricinfra
-  log_command sudo kubectl create ns ricinfra
-  log_command sudo helm install stable/nfs-server-provisioner --namespace ricinfra --name nfs-release-1
-  log_command sudo kubectl patch storageclass nfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-  log_command sudo apt-get install nfs-common
-  log_command sudo docker run -d -p 5001:5000 --restart=always --name ric registry:2
-  print_progress "Git repository cloned and setup successfully."
+setup_git() {
+  print_progress "Setting up Git repository..."
+  log_command git clone https://github.com/openaicellular/oaic.git oaic
+  log_command git -C oaic submodule update --init --recursive --remote
+  print_progress "Git repository setup successfully."
 }
 
-install_packages_with_docker() {
-  print_progress "Installing packages using Docker..."
-  log_command cd ../../../..
-  log_command cd ric-plt-e2
-  log_command cd RIC-E2-TERMINATION
-  log_command sudo docker build -f Dockerfile -t localhost:5001/ric-plt-e2:5.5.0 .
+configure_k8s() {
+  print_progress "Configuring Kubernetes..."
+  log_command sudo ./oaic/RIC-Deployment/tools/k8s/bin/gen-cloud-init.sh
+  log_command sudo ./oaic/RIC-Deployment/tools/k8s/bin/k8s-1node-cloud-init-k_1_16-h_2_17-d_cur.sh
+  log_command sudo apt-get install nfs-common
+  log_command sudo docker run -d -p 5001:5000 --restart=always --name ric registry:2
+  print_progress "Kubernetes configured successfully."
+}
+
+deploy_docker() {
+  print_progress "Deploying Docker containers..."
+  log_command sudo docker build -f ./oaic/ric-plt-e2/RIC-E2-TERMINATION/Dockerfile -t localhost:5001/ric-plt-e2:5.5.0 .
   log_command sudo docker push localhost:5001/ric-plt-e2:5.5.0
-  log_command cd ../../
-  log_command cd RIC-Deployment/bin
-  log_command sudo ./deploy-ric-platform -f ../RECIPE_EXAMPLE/PLATFORM/example_recipe_oran_e_release_modified_e2.yaml
-  print_progress "Packages installed using Docker successfully."
+  log_command sudo ./oaic/RIC-Deployment/bin/deploy-ric-platform -f ./oaic/RIC-Deployment/RECIPE_EXAMPLE/PLATFORM/example_recipe_oran_e_release_modified_e2.yaml
+  print_progress "Docker containers deployed successfully."
 }
 
 # Welcome message
-print_progress "Welcome to the OAIC-Project Installation!"
+echo -e "\e[1;34m"
+cat << "EOF"
+  ____                   _    _    _     _   
+ / ___|  ___   ___  _ __| |_ / |_ | |__ (_)_ 
+ \___ \ / _ \ / _ \| '__| __| | __|| '_ \| \ \
+  ___) |  __/|  __/| |  | |_| | |_ | | | | |\ \
+ |____/ \___| \___||_|   \__|_|\__||_| |_|_| \_\
+                                                
+EOF
+echo -e "\e[0m"
+echo -e "Welcome to \e[1;32mOAIC Setup\e[0m!"
+echo -e "Let's get started...\n"
 
-# Start the installation steps
 update_packages
-clone_and_setup_git
-install_packages_with_docker
+setup_git
+configure_k8s
+deploy_docker
 
-# Done message
-print_progress "OAIC-Project setup completed successfully!"
+print_progress "OAIC RIC setup completed successfully!"
